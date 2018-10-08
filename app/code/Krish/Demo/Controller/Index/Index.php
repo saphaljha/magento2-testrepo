d9bf2058be9dfaa33fee9c7fc62ff58421c406fb
saphal <saphal.jha@krishtechnolabs.com>
2018-10-08 13:04:12 +0530

    test controller

---
+++ app/code/Krish/Demo/Controller/Index/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace Krish\Demo\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+	public function execute()
+	{
+		echo 'test';
+		die('***---***');
+	}
+}
